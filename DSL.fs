03b8ae62ce5fa66d9304c4913a7a65e18664b44a
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-02-10 20:21:13 +0100

    Added definitions for zcb1, zcb2, european call, american call, and forward. Deleted ambiguous contracts.

--- DSL.fs
+++ DSL.fs
@@ -3,11 +3,13 @@ open System
 // Firstly, I need to define a languague that describes what a contract is. 
 // It is basically an agreement between 2 parts to exchange something at a certain time. 
 // The DSL is currently mostly inspired by the research article "Composing Contracts: An Adventure in Finanical Engineering, by Simon Peyton Jones, Jean-Marc Eber and Julian Seward"
-type Currency = USD | EUR | DKK | None  
+type Currency = USD | EUR | GDP | DKK | None
+
 type Obs = 
-  | Value of double
-  | Underlying of double
+  | Value of double // how many
+  | Underlying of string * DateTime
   | Strike of double
+  | AveragePrice of double // used for Asian Contracts
   | Mul of Obs * Obs
   | Add of Obs * Obs
   | Sub of Obs * Obs
@@ -18,39 +20,73 @@ type Contract =
   | Scale of Obs * Contract 
   | All of Contract list
   | FixingDate of DateTime
-  | MaturityDate of DateTime
-  | Acquire of Contract
-  | Give of Contract
-  | And of Contract * Contract
+  | MaturityDate of DateTime   
+  | Acquire of DateTime * Contract
+  | Give of Contract // need datetime aswell here imo
   | Or of Contract * Contract
-  | Truncate of DateTime * Contract // Trims the contracts horizon so that it cannot be acquired before the DateTime.
-  | European of DateTime * Contract // you can acquire a underlying contract at time T 
+ // | Truncate of DateTime * Contract // Trims the contracts horizon so that it cannot be acquired before the date.
+ // Tbh I dont really see right now why Truncate is needed.
   | Anytime of Contract // used for American options
-  | Then of Contract * Contract // Then(c_1, c_2) means you acquire c_1 if it has not expired; else c2.
+  | Then of Contract * Contract // Then(c1, c2) means you acquire c1 if it has not expired; else c2.
 
-// Example 0: A contract cointaining nothing.
-let c0 : Contract = Scale(Value(0.0), One((None)))
-// Example 1: Zero Coupond Bond.  Recieve 100DKK on 22th of February 2000, if you acquire the contract before 22th of February 2000.
-let c1 = Acquire(Truncate(DateTime(2000, 2, 22), Scale(Value(100.0), One(DKK))))
+
+// Example 0: A contract cointaining nothing. That is: A value of 0 with no currency.
+let c0: Contract =  Scale(Value(0.0), One((None)))
+let c0_: Contract = All [] 
+let flow(d: string, v: double, c: Currency) : Contract = Acquire(System.DateTime.Parse d,Scale(Value(v), One(c)))  
+(* Acquire a bond that pays 100 USD on the 1st of December 2023. *)
+let zcb1: Contract = 
+  let maturityDate: string = "2000-1-12"
+  flow(maturityDate, 100.0, USD)
+
+(* Example: Acquire a bond that pays 100 USD on the 1st of December 2023 and pays dividends of 10 USD on the 1st of every quarter. *)
+let dividendPayingBond: Contract = 
+  let maturityDate: string = "2000/12/1"
+  let pct: float = 0.02
+  let qPct : float = exp(pct * 1.0/4.0)
+  let investment: float = 100.0
+  let quarterlyPayments: Contract = 
+    let payment: float = qPct * investment
+    All([
+        flow("2000/3/1", payment, USD);
+        flow("2000/6/1", payment, USD);    
+        flow("2000/9/1", payment, USD);    
+        flow("2000/12/1", payment, USD)])
+  Then(quarterlyPayments, flow(maturityDate, investment, USD))
+
 // Example 2: Zero Coupond Bond 2. Recieve 200USD on 1st of March 2000.
-let c2 = Acquire(Truncate(DateTime(2000, 3, 1), Scale(Value(100.0), One(USD))))
-// Example 3: A contract that involves c1 and c2.
-let c3 = And(c1, c2)
-// Example 4: A contract whose holder receives 100DKK at t=1 and pays 200USD at t=2.
-let c4 = And(c1, Give(c2))
-// Example 5: Gives the holder immediately 1EUR with infinite horizon.
-let c5 = One(EUR)
-// Example 6: Gives the holder immediately 100EUR with infinite horizon.
-let c6 = Scale(Value(100.0), c5)
-// Example 7: skipped
-let c7 = One(USD)
-// Example 8: European Contract. You can acquire v1 (Contract) on t if you want, but you need to give v2 (Contract). Can be money. Otherwise, nothing happens (c0)
-let c8(t, v1 : Contract, v2 : Contract) = European(t, Or((And(v1, Give(v2))), c0))
-// Example 9: American option.
-let perhaps(t, u: Contract) = Truncate(t, Or(u, Scale(Value(0.0),One(None))))
-//let c9(t1 : DateTime, t2 : DateTime) = Acquire(Truncate(t1))
-// Define american contract here:
-let american = 0
+let zcb2: Contract = Acquire(DateTime(2000, 3, 1), Scale(Value(100.0), One(USD)))
+// Example 3: A contract that involves zcb1 and c2.
+let c3: Contract = All([zcb1; zcb2])
+// Example 4: A contract whose holder receives 100DKK on 22th of February 2000 and pays 200USD at 1st of March 2000.
+let c4: Contract = All([zcb1; Give(zcb2)])
+// Example 8: 10 European Call Option acquired 2000/2/22, with expiry date 2000/3/1 with Vestas as underlying and a strike of 100.0.
+let europeanCall: Contract =
+  let acquireDate: DateTime = DateTime(2000, 2, 22)
+  let expiryDate: DateTime = DateTime(2000, 3, 1)
+  let equity: string = "Vestas Wind Systems A/S"
+  let strike: float = 100.0
+  let nominal: float = 10.0
+  let obs: Obs = Max(Value(0.0), Sub(Underlying(equity, expiryDate), Value(strike)))
+  Scale(Value(nominal), Acquire(expiryDate,Scale(obs,One(DKK))))
+
+let americanCall: Contract = 
+  let acquireDate: DateTime = DateTime(2000, 2, 22)
+  let expiryDate: DateTime = DateTime(2000, 3, 1)
+  let equity: string = "Vestas Wind Systems A/S"
+  let strike: float = 100.0
+  let units: float = 10.0
+  let obs: Obs = 
+    Max(Value(0.0), Sub(Underlying(equity, expiryDate), Value(strike)))
+  Anytime(Acquire(expiryDate, Scale(Value(units), Scale(obs, One(DKK)))))
+
+let forward: Contract = 
+  let acquireDate: DateTime = DateTime(2000, 2, 22)
+  let expiryDate: DateTime = DateTime(2000, 3, 1)
+  let equity: string = "Vestas Wind Systems A/S"
+  let price: float = 100.0
+  let nominal: float = 10.0
+  let obs: Obs = Sub(Underlying(equity, expiryDate), Value(price))
+  Scale(Value(nominal), Acquire(expiryDate, Scale(obs,One(DKK))))
 
 // Example n: Forward contract. Recieve S_T-K at time T; not before. Say, T = 22th of February 2000, S_T = 100DKK and K = 50DKK
-let cn = Acquire(Truncate(DateTime(2000, 2, 22), Scale(Sub(Underlying(100.0), Strike(50.0)), One(DKK))))
