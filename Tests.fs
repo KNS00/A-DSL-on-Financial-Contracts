4a78c11f5b2674182fe2eaa3383b10142e85b799
Kasper Schiller <88677877+KNS00@users.noreply.github.com>
2023-03-19 17:25:48 +0100

    Update Tests.fs
    changed comment

--- Tests.fs
+++ Tests.fs
@@ -37,4 +37,4 @@ printfn "%s %A" "Test Evaluation of contract3:" result3 // 9.99,   correct
 printfn "%s %A" "Test Evaluation of contract4:" result4 // 98.02,  correct
 printfn "%s %A" "Test Evaluation of contract5:" result5 // 209.02,   correct
 printfn "%s %A" "Test Evaluation of call option:" result6 // Should be around 115 beacuse E[S_T] = 165 and 165 - 50 = 115. However, this will
-                                                          // not work for exotic options! Because E[F(S_T)] != F(E[S_T]) for non-linear functions!
+                                                          // not work for exotic options! Because obviously E[F(S_T)] != F(E[S_T]) for non-linear functions!
