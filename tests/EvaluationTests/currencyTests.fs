afb8855f1d8045c71ce8c0f9ff5212753e2b6570
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-05-03 12:01:45 +0200

    Updated currency tests!! Now you can run it with —filter

--- tests/EvaluationTests/currencyTests.fs
+++ tests/EvaluationTests/currencyTests.fs
@@ -6,40 +6,18 @@ open Analysis
 open FsUnit
 open Xunit
 
-let testEvalccy(testCases : (Currency * float) list) : unit =
-    testCases
-    |> List.iter (fun (input, expectedOutput) ->
-        evalccy input |> should equal expectedOutput)
-
-type CurrencyRate = {
-    Currency: Currency
-    Rate: float
-}
-
-let evalccyTestCases : seq<(Currency * float)> =
-    seq {
-        (EUR, 1.10)
-        (GBP, 1.24)
-        (DKK, 0.15)
-        (USD, 1.10)
-    }
+let evalccyTestCases : List<obj[]> =
+    [   
+        [| EUR; 1.10 |]
+        [| GBP; 1.24 |]
+        [| DKK; 0.15 |]
+        [| USD; 1.0 |]
+    ]
 
 type TestCases =
     static member EvalccyTestCases = evalccyTestCases
 
-
-
 [<Theory>]
-[<MemberData(nameof(evalccyTestCases), MemberType = typeof<TestCases>)>]
+[<MemberData(nameof(TestCases.EvalccyTestCases), MemberType = typeof<TestCases>)>]
 let ``evalccy should evaluate currencies correctly``(ccy: Currency, expectedRate: float) =
     evalccy ccy |> should equal expectedRate
-
-
-
-    (*let evalccyTestCases : (Currency * float) list =[
-    EUR, 1.10;
-    GBP, 1.24;
-    DKK, 0.15;
-    USD, 1.10;
-    ]
-    *)
