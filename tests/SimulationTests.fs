c1e348266318f4fcbc08f232df266fd4cf45dc3c
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-05-08 14:33:51 +0200

    add test case for introducing the xUnit framework in the report. moved listsAreEqual to testFunctions.fs.

--- tests/SimulationTests.fs
+++ tests/SimulationTests.fs
@@ -1,4 +1,5 @@
 ﻿module simulationTests
+open testFunctions
 open Domain
 open Evaluations
 open Simulations
@@ -7,19 +8,6 @@ open FsUnit
 open Xunit
 
 module simulationTests =
-    let listsAreEqual (xs: float list) (ys: float list) (tolerance: float) : bool = // checks if two lists are approximately equal
-        match List.length xs, List.length ys with
-        | xlen, ylen when xlen <> ylen -> failwith "Lists must have the same length"
-        | _ -> xs |> List.forall2 (fun x y -> abs(x - y) / y <= tolerance) ys
-    
-    let simulation (f: 'a -> float) (input : 'a) : float = // runs a function 1_000_000 times and returns the average output
-        let n = 1_000_000
-        [for _ in 1..n ->
-            let res = f input
-            res]
-        |> List.average
-        
-    
     [<Theory>] 
     [<InlineData(0, 1, 1.0)>]
     [<InlineData(10, 15, 1.0)>]    
@@ -47,7 +35,6 @@ module simulationTests =
     [<InlineData(15.0, 0, 10, 1.0, -0.2, 0.4)>]     // S_0 = 15.0, start = 0 end = 10, dt = 1.0, mu = 0.5, sigma = 0.4
     //[<InlineData(50.0, 10, 15, 1.0, 0.1, 0.1)>]  // S_0 = 50.0, start = 10, end = 11, dt = 0.01, mu = -0.5, sigma = 0.1
     [<InlineData(1.0, 0, 15, 1.0, 1.0, 0.2)>]      // S_0 = 1.0, start = 0, end = 3, dt = 0.07, mu = 0.0, sigma = 0.6
-
     let ``Assert GBM Expected Value``(S0, startTime, endTime, dt, mu, sigma) =
         let expectation t = S0 * exp(mu * t) // E[S_t] = S_0 * exp(mu * t)
         let expectedValues : float list =
@@ -70,5 +57,14 @@ module simulationTests =
        
        
 
+    let add(x : float, y : float) 
+        = x + y
 
-       
+
+    [<Theory>]
+    [<InlineData(1.0, 1.0)>]
+    [<InlineData(5.0, 25)>]
+    [<InlineData(-5.7, 32.49)>]
+    let ``test``(input : float * float) (expectedOutput : float) : unit =
+        add(input) |> should (equalWithin 1e-7) expectedOutput
+    
