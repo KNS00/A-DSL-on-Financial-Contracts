6fbce52897158f9dae30bd666e06c1e3e4d9d6ef
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-05-14 12:33:15 +0200

    updated monte carlo simulation

--- src/Analysis.fs
+++ src/Analysis.fs
@@ -2,6 +2,7 @@ module Analysis
 open Domain
 open Evaluations
 open Simulations
+open XMLFunctions
 //open exchangeRates
 
 /// <summary>
@@ -10,20 +11,23 @@ open Simulations
 /// </summary>
 /// <param name="c1">The contract to simulate.</param>
 /// <returns>The expected value of the option.</returns>
-let simulateContract (c1 : Contract) : float =
-    let sims = 10
-    let E (s : string, t : int) : float = 
-        let resultMap = makeE [s] t 1.0
-        match resultMap |> Map.tryFind (s, t) with
-        | Some value ->
-            printfn "%s %A " "stock price" value
-            value
-        | None -> failwith "Stock was not found"
+let simulateContract (c : Contract) : float =
+    let sims = 100_000 /// change this to input variable
+    let underlyings : string list = getStocks(c)
+    let price : float = getPrice (List.head underlyings) 0
+    printfn "%s %A" "underlying price:" price
+    let maturity = getMaturityDate(c)
     let evaluations : float list =
         [for _ in 1..sims ->
-            let res = evalc I E c1
-            printfn "%s %A" "evaluation" res
-            res]
-    evaluations |> List.average
+            let resultMap = makeE underlyings maturity 1.0
+            let E(s,t) : float = Map.find(s, t) resultMap
+            printfn "%A" resultMap
+            printfn "%A" (E("DIKU A/S", 30))
 
+            let res = evalc I E c
+            printfn "%A" res
+            res]
+    printfn "%s %A" "evaluations" evaluations
+    printfn "%s %A" "sum of evaluations" (List.sum evaluations)
+    evaluations |> List.average
 
