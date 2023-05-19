f2d8f0451297390794dafd8162222fbf3b375a82
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-05-19 11:43:04 +0200

    Moved the simulation of contracts into Simulations.fs.

--- src/Analysis.fs
+++ src/Analysis.fs
@@ -5,23 +5,5 @@ open Simulations
 open XMLFunctions
 //open exchangeRates
 
-/// <summary>
-/// Runs a Monte Carlo simulation to estimate the expected value of a given contract.
-/// Uses the given options underlying stocks to generate stock price before simulation.
-/// </summary>
-/// <param name="c1">The contract to simulate.</param>
-/// <returns>The expected value of the option.</returns>
-let simulateContract (sims : int) (c : Contract) : float =
-    let underlyings : string list = getStocks(c)
-    let maturity = getMaturityDate(c)
-    let evaluations : float list =
-        [for _ in 1..sims ->
-            let resultMap = makeE underlyings maturity 1.0
-            let E(s,t) : float = Map.find(s, t) resultMap
-            let res = evalc I E c
-            //printfn "%A" res
-            res]
-    //printfn "%s %A" "evaluations" evaluations
-    //printfn "%s %A" "sum of evaluations" (List.sum evaluations)
-    evaluations |> List.average
 
+// plots
