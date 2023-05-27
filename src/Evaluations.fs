b1c7cd672819d117c0ce8314c71e19984cc5715b
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-05-27 23:12:34 +0200

    update evaluation functions with new names for contract information functions

--- src/Evaluations.fs
+++ src/Evaluations.fs
@@ -1,5 +1,6 @@
 ﻿module Evaluations
 open Domain
+open Management
 /// <summary>
 /// Evaluates a currency to USD.
 /// </summary>
@@ -21,26 +22,26 @@ let evalccy (curr: Currency) : float =
 /// <param name="E">The function used to evaluate stock prices.</param>
 /// <param name="o">The observable to evaluate.</param>
 /// <returns>The evaluated observable value as a float.</returns>
-let rec evalo (E:(string*int)->float) (o : Obs) : float = 
-  match o with
-  | Value n -> n
-  | Underlying (s, t) -> E(s,t)
-  | Mul (c1, c2) ->
-      let n1 = evalo E c1
-      let n2 = evalo E c2
-      n1 * n2
-  | Add (c1, c2) ->
-      let n1 = evalo E c1
-      let n2 = evalo E c2
-      n1 + n2
-  | Sub (c1, c2) ->
-      let n1 = evalo E c1
-      let n2 = evalo E c2
-      n1 - n2
-  | Max (c1, c2) ->
-      let n1 = evalo E c1
-      let n2 = evalo E c2
-      max n1 n2
+let rec evalo (E:(string*int)->float) (o : float) : float = 
+    match o with
+    | Value n -> n
+    | Underlying (s, t) ->E(s,t)
+    | Mul (c1, c2) ->
+        let n1 = evalo E c1
+        let n2 = evalo E c2
+        n1 * n2
+    | Add (c1, c2) ->
+        let n1 = evalo E c1
+        let n2 = evalo E c2
+        n1 + n2
+    | Sub (c1, c2) ->
+        let n1 = evalo E c1
+        let n2 = evalo E c2
+        n1 - n2
+    | Max (c1, c2) ->
+        let n1 = evalo E c1
+        let n2 = evalo E c2
+        max n1 n2
 
 /// <summary>
 /// Evaluates a given contract.
@@ -51,11 +52,11 @@ let rec evalo (E:(string*int)->float) (o : Obs) : float =
 /// <returns>The evaluated contract as a float.</returns>
 let rec evalc (I:float->int->float) (E:(string*int)->float) (c: Contract) : float =
   match c with
-  | One c -> evalccy c // evaluate currency
+  | One c -> evalccy c  // evaluate currency
   | Scale (obs, c1) -> evalo E obs * evalc I E c1 
   | All [] -> 0.0
   | All (c1::cs) -> evalc I E c1 + evalc I E (All cs)
   | Acquire(r, t, c1) -> I r t * evalc I E c1 
   | Or(c1, c2) -> evalc I E c1 + evalc I E c2 
   | Give(c1) -> -1.0 * evalc I E c1 
-  | Then(c1, c2) -> if getMaturityDate(c1) > 0 then evalc I E c1 else evalc I E c2
+  | Then(c1, c2) -> if maturity(c1) > 0 then evalc I E c1 else evalc I E c2
