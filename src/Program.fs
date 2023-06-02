d40e5a664b079ca90d750f812413518a3b9e1e7d
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-06-02 18:49:55 +0200

    added stuff to Program.fs for testing

--- src/Program.fs
+++ src/Program.fs
@@ -16,32 +16,56 @@ open System
 let plotsDict = 
     dict [
         "plotWienerProcess", (fun () -> plotWienerProcess())
-        "plotGBM", (fun () -> plotGBM())
+        "plotAsian", (fun () -> plotAsian())
         "plotEuropeanCall", (fun () -> plotEuropeanCall())
     ]
 
 [<EntryPoint>]
 let main argv =
+    printfn "%A" sec1
+    printfn "%A" (blackScholes())
+    
+    let gbm() : float = List.last (GBMPath 100.0 3650 0.01 0.02 0.002)
+    printfn "%A" (List.init 10_000 (fun _ -> gbm()) |> List.average)
+
+
+
+    let contractio =
+        All [
+            Acquire(5, Acquire(10, Scale(Underlying("AAPL", 3), One USD)));
+           // Acquire(10, Scale(Underlying("AAPL", 2), One USD))
+           Acquire(15, Or(Acquire(10,Scale(Value 100.0, One DKK)), Acquire(10, Scale(Value 100.0, One DKK))))
+        ]
+
+    let cflows = flows contractio
+
+
+
     printfn "start %A" contractio
     printfn "after %A" cflows
-    //printfn "more simple %A" moresimple
-    //blackScholes()
-    //let value = simulateContract 1_000 0.01 EuropeanCallOption
-    //printfn "%s %A" "value of call DSL simulation" value
+    
 
+    //printfn "more simple %A" moresimple
+    let seed = 1
+    Random.SetSampleGenerator(Random.RandThreadSafe(seed))
+    //printfn "black scholes %A" (blackScholes())
+    //let sim = simulateContract 10_000 EuropeanCallOption
+    //printfn "sim %A value" sim
     let random = new Random()
     let mutable seed = random.Next()
     let parseSeed argv = // check if the user has given a seed as the first argument or not
         match argv with
         | (arg: string) :: args when Int32.TryParse(arg, &seed) -> seed, args
         | args -> random.Next(), args
-
-    let (seed, args) = parseSeed (List.ofArray argv)
-    printfn "Using seed %i." seed
+    let (seed, args) = parseSeed (List.ofArray argv) 
+    //let cc : (string * int) List = underlyings (Acquire(0.02, 10, Scale(Underlying("AAPL", 10), One DKK))) 0
+    //printfn "%A" (simulateContract 10_000 EuropeanCallOption)
+    //printfn "black scholes %A" (blackScholes())
+    //printfn "Using seed %i." seed
     Random.SetSampleGenerator(Random.RandThreadSafe(seed))
 
-    // iterate through the arguments given by the user
-    args 
+    //iterate through the arguments given by the user
+    args
     |> List.iter (fun arg ->
         match plotsDict.TryGetValue arg with
         | (true, func) -> 
