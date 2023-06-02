2d828df48954d2894c0eb9d5996b913d8c002cc7
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-06-02 18:44:51 +0200

    changed all the examples in section 3 in the report, changed the code accordingly

--- src/Examples.fs
+++ src/Examples.fs
@@ -1,96 +1,57 @@
 ﻿module Examples
 open Domain
-open Evaluations
-open Simulations
-open Analysis
 
 
-let i = 0.02
+let flow(t: int) (v: double) (c: Currency) : Contract
+    = Acquire(t, Scale(Value v, One c))
 
-let flow(i : float, t: int, v: double, c: Currency) : Contract
-    = Acquire(i, t, Scale(Value v, One c))
+let zcb (T : int) (face : float) (ccy : Currency) : Contract = 
+  flow T face ccy
 
-let zcb1: Contract = 
-  let maturityDate : int = 10
-  flow(i, maturityDate, 100.0, USD)
+// the maturity input is in days
+let cb (T : int) (face : float) (rate : float) (yearlyFreq : float) (ccy : Currency) : Contract =
+    let dates = int(yearlyFreq * 365.)
+    let rateDates = [dates .. dates .. T] |> List.ofSeq
+    let couponFlows : Contract List = List.map (fun x -> flow x (face * rate / yearlyFreq) ccy) rateDates
+    let faceFlow = zcb T face ccy
+    All(faceFlow :: couponFlows)
 
-
-let dividendPayingBond : Contract = 
-  let maturityDate = 120
-  let pct = 0.02
-  let qPct : float = exp(pct * 1.0/4.0)
-  let investment = 100.0
-  let quarterlyPayments: Contract = 
-    let payment = qPct * investment
+let ccSwap (T : int) (ccA : Currency) (ccB : Currency) (nA : float) (nB : float) (rA : float) (rB : float) (yearlyFreq : float) : Contract =
+    let dates = int(yearlyFreq * 365.)
+    let rateDates = [365 / dates .. 365  .. T]
+    let interestPaymentsA = List.map (fun x -> Give(flow x (nA * rA / yearlyFreq) ccA)) rateDates
+    let interestPaymentsB = List.map (fun x -> flow x (nB * rB / yearlyFreq) ccB) rateDates
     All([
-        flow(i, 30, payment, USD);
-        flow(i, 60, payment, USD);    
-        flow(i, 90, payment, USD);    
-        flow(i, 120, payment, USD)])
-  Then(quarterlyPayments, flow(i, maturityDate, investment, USD))
+        Give(flow 0 nA ccA); // Inception: give nA units of currency ccA.
+        flow 0 nB ccB;       // Inception: acquire nB units of currency ccB.
+        All(interestPaymentsA); // Pay interest at intervals
+        All(interestPaymentsB); // Receive interest at intervals
+        Give(flow T nB ccB); // At maturity, pay back nB units of currency ccB.
+        flow T nA ccA        // At maturity, acquire back nA units of currency ccA.
+    ])
 
 
-(* Sell a loan with a principal of 100, where the buyer pays you 
-30 USD every quarter for a year.  *)
-let amortizedLoan = 
-    let principal = 100.0
-    let coupon = 30.0
-    All([Give(flow(i, 0, principal, USD));
-        flow(i, 91, coupon, USD);
-        flow(i, 182, coupon, USD);    
-        flow(i, 273, coupon, USD);    
-        flow(i, 364, coupon, USD)])    
-
-(* A contract that includes the acquisition of a European Call 
-Option with the underlying stock AAPL, a strike price of 100 
-dollars and maturity in 30 days. *)
-let EuropeanCallOption : Contract =
-    let underlying = "AAPL"
-    let strike = 100.0 
-    let maturity = 30
-    let ccy = USD
+let europeanCall (T : int) (stock : string) (strike : float )  (ccy : Currency) : Contract =
     let payoff = 
         Max(Value 0.0,
-            Sub(Underlying(underlying, maturity), 
+            Sub(Underlying(stock, 0), 
                 Value strike))
-    Acquire(i, maturity, Scale(payoff, One ccy))
+    Acquire(T, Scale(payoff, One ccy))
 
-let exampleEuropeanPutOption : Contract =
-    let underlying = "DIKU A/S"
-    let strike = 100.0 
-    let maturity = 30
-    let currency = USD
+let europeanPut (T : int) (u : string) (strike : float )  (ccy : Currency) : Contract =
     let payoff = 
-        Max(Value 0.0, Sub(Value (strike * I i maturity), Underlying(underlying, maturity)))
-    Scale(payoff, One currency)
+        Max(Value 0.0,
+            Sub(Value strike,
+                Underlying(u, 0)))
+    Acquire(T, Scale(payoff, One ccy))
 
-let exampleForward : Contract =
-    let underlying = "DIKU A/S"
-    let strike = 100.0 
-    let maturity = 30
-    let currency = USD
-    let payoff = Sub(Underlying(underlying, maturity), Value (strike * I i maturity))
-    Scale(payoff, One currency)
+let forward (T : int) (u : string) (strike : float )  (ccy : Currency)  : Contract =
+    let payoff = 
+        Sub(Value strike,
+            Underlying(u, 0))
+    Acquire(T, Scale(payoff, One ccy))
 
-
-let exampleCurrencySwap : Contract =
-    let notional1 = 1000000.0
-    let currency1 = USD
-    let notional2 = 900000.0
-    let currency2 = EUR
-    let maturity = 30
-
-    let leg1 = Scale(Value notional1, One currency1)
-    let leg2 = Scale(Value notional2, One currency2)
-    Then(leg1, Acquire(i, maturity, leg2))
-
-let exampleFixedRateBond : Contract =
-    let principal = 1000.0
-    let rate = 0.05
-    let maturity = 30
-    let currency = USD
-    let coupon = Scale(Value (principal * rate), One currency)
-    let principalAtMaturity = Scale(Value principal, One currency)
-    All [coupon; Acquire(i, maturity, principalAtMaturity)]
-    
-
+let chooser(t : int) (T : int) (stock : string) (strike : float) (ccy : Currency) : Contract =
+    let ec = europeanCall T stock strike ccy
+    let ep = europeanPut T stock strike ccy
+    Acquire(t, Or(ec, ep)) // remember: the maturity of ec or ep is then t+T.
