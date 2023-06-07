944870fcd5d200d654f56e9f6c7a8d86a1e03dbd
kapzyboy <88677877+kapzyboy@users.noreply.github.com>
2023-06-07 14:40:48 +0200

    fix bugs in simulation functions

--- src/Simulations.fs
+++ src/Simulations.fs
@@ -15,14 +15,12 @@ open Examples
 
 let normal = ContinuousDistribution.normal 0.0 1.0
 
-
-let f (ccy : Currency) : float =
-    match ccy with
+let f (c : Currency) : float =
+    match c with
     | USD -> 1.0
     | EUR -> 1.10
     | GBP -> 1.24
     | DKK -> 0.15
-
 
 /// <summary>
 /// Discounts the value 1 back in time according to the interest rate.
@@ -30,7 +28,6 @@ let f (ccy : Currency) : float =
 /// <param name="i">The time period in days.</param>
 /// <returns>The value 1 discounted to the current time.</returns>
 let I (t : int) : float =  
-    //let rDaily : float = exp(r * float t / 365.0) - 1.0 // conversion from annual to daily rate
     let r = 0.02
     let presentValue : float = exp(-r/365. * (float t)) // e^{-rt}
     presentValue
@@ -44,13 +41,12 @@ let I (t : int) : float =
 let WienerProcess(T : int) (dt : float) : float list =
     let numSteps = int(ceil((float T) / dt))
     let sampleValues = List.init numSteps (fun _ -> normal.Sample() * sqrt(dt))
-    let results = List.scan (+) 0.0 sampleValues // fix : only prior value should be added
+    let results = List.scan (+) 0.0 sampleValues 
     results
 
 
 let GBM (initialPrice : float) (T : int) (mu : float) (sigma : float) : float =
     let Td = float T/365.
-    let normal = ContinuousDistribution.normal 0.0 1.0
     let drift = (mu - 0.5 * (sigma ** 2.0)) * float Td
     let diffusion = sigma * sqrt(float Td) * normal.Sample()
     initialPrice * exp(drift + diffusion)
@@ -67,22 +63,17 @@ let GBM (initialPrice : float) (T : int) (mu : float) (sigma : float) : float =
 /// <param name="wpValues">A list of floats that represent a simulated Wiener Process.</param>
 /// <returns>A list of floats that represent the Geometric Brownian Motion.</returns>
 let GBMPath (initialPrice: float) (T: int) (dt: float) (mu: float) (sigma: float) : float list =
-    let normal = ContinuousDistribution.normal 0.0 1.0
-    let Td = float T / 365.0
-    let numSteps = int (float Td / dt)
-    let drift = (mu - 0.5 * sigma**2.) * dt
-    let diffusion = sigma * sqrt(dt)
+    let numSteps = int (float T / dt)
+    let dt_ = dt / 365.0
+    let drift = (mu - 0.5 * sigma**2.) * dt_
+    let diffusion = sigma * sqrt(dt_)
 
     let simulateSteps (initialPrice: float) : float list =
-        // Generate the Wiener process
-        let wienerProcess = List.init numSteps (fun _ -> normal.Sample() * sqrt(dt))
-        // Scan the Wiener process to compute the GBM
-        let prices = List.scan (fun price dW -> price * exp(drift + diffusion * dW)) initialPrice wienerProcess
+        let Z = List.init numSteps (fun _ -> normal.Sample() * sqrt(dt_))
+        let prices = List.scan (fun price dW -> price * exp(drift + diffusion * dW)) initialPrice Z
         prices
 
     simulateSteps initialPrice
-
-
 
 /// <summary>
 /// Simulates a stock from now until the given time based on the current price, drift and volatility found in a XML file.
@@ -131,8 +122,3 @@ let simulateContract (sims : int) (c : Contract) : float =
     //printfn "%s %A" "sum of evaluations" (List.sum evaluations)
     evaluations |> List.average
 
-
-let ec1 = europeanCall1 10 "AAPL" 95. USD
-let sec1 = simulateContract 10_000 ec1
-
-let co1 = chooser 0 10 "AAPL" 95. USD
