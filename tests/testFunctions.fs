1c8dd17cda88e650231916a20925e7f69d9bb89d
Kasper Schiller <88677877+KNS00@users.noreply.github.com>
2023-06-07 14:43:34 +0200

    Delete tests directory

--- tests/testFunctions.fs
+++
@@ -1,6 +0,0 @@
-﻿module testFunctions
-
-let listsAreEqual (xs: float list) (ys: float list) (tolerance: float) : bool = // checks if two lists are approximately equal
-    match List.length xs, List.length ys with
-    | xlen, ylen when xlen <> ylen -> failwith "Lists must have the same length"
-    | _ -> xs |> List.forall2 (fun x y -> abs(x - y) / y <= tolerance) ys
