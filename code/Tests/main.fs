f8a3969c4d45becf079cf6ea25d229ef32d10b4a
Kasper Schiller <88677877+KNS00@users.noreply.github.com>
2025-01-09 15:19:56 +0100

    Removed arbitrary example from the main unit test function

--- code/Tests/main.fs
+++ code/Tests/main.fs
@@ -2,14 +2,5 @@
 open Xunit
 
 module testProgram =
-    let squared(x : float) 
-        = x**2.0
-    [<Theory>]
-    [<InlineData(1.0, 1.0)>]
-    [<InlineData(5.0, 25)>]
-    [<InlineData(-5.7, 32.49)>]
-    let ``test square``(input : float) (expectedOutput : float) : unit =
-        squared(input) |> should (equalWithin 1e-7) expectedOutput
-
     let [<EntryPoint>] main _ =
         0
